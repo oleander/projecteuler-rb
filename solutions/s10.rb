b5589cd1354221993c1b08ef77362dc824233d49
Linus Oleander <linus@oleander.nu>
2011-12-30 21:45:07 +0100

    Minor change in s10

--- solutions/s10.rb
+++ solutions/s10.rb
@@ -1,12 +1,13 @@
 def is_prime?(n)
+  return true if n == 2
   return false if n.even?
   return false if n < 2
-  return true if n == 2
   !! (2..(n-1)).each {|i| return false if (n % i).zero?}
 end
 
 puts (0..2_000_000).inject(0) {|res, n| res + (is_prime?(n) ? n : 0)}
-# => 142913828920
+# => 142913828922
+
 # real 402m11.564s
 # user 379m26.622s
 # sys 1m8.815s
